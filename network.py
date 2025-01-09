879e835ee38b332ebc7776429d9d2cbbcac567c1
hkwln <p.aul@mail.de>
2025-01-09 13:53:54 +0100

    added a torch module

--- network.py
+++ network.py
@@ -1,9 +1,22 @@
 import torch
 
-x = torch.tensor([0,0,1,0], dtype=torch.float) # 4-dim float vector
-print(x)
+class SimpleNN(torch.nn.Module):
+    def __init__(self, input_dim: int):
+        super().__init__()
+        # define the layers
+        
 
-Layer = torch.nn.Linear(4, 5, bias= True) # 4-dim input, 5-dim output -> 5 neurons
-
-z1 = Layer(x)   # forward pass
-y1 = torch.sigmoid(z1)
+        # create input layer: 5 neurons
+        self.layer1 = torch.nn.Linear(input_dim, 5)
+        # hidden layer with 3 neurons
+        self.layer2 = torch.nn.Linear(5, 3)
+        # output layer with 1 neuron for binary classification
+        self.layer3 = torch.sigmoid(torch.nn.Linear(3, 1))
+        
+    def forward(self, x):
+        # define the forward pass
+        y1 = torch.relu(self.layer1(x))
+        y2 = torch.relu(self.layer2(y1))
+        y3 = torch.sigmoid(self.layer3(y2))
+        
+        return y3
