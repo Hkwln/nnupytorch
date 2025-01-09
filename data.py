f2a8e7443453f174e06380637ba15c5bc39277da
hkwln <p.aul@mail.de>
2025-01-09 16:57:36 +0100

    encode does not work yet

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 import os
+import numpy as np
 
 
 dataset_folder = os.path.join(os.getcwd(), 'dataset')
@@ -24,3 +25,8 @@ vocab_size = len(chars)
 print(vocab_size)
 print(chars)
 # Create a mapping from characters to integers
+char_to_int ={char: i for i, char in enumerate(chars)}
+int_to_char = {i: char for i, char in enumerate(chars)}
+encode = np.array([char_to_int[char] for char in all_text], dtype=np.uint8)
+
+print(encode("Guten Morgen"))
