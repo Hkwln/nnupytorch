1a7c7fd5420ab6b3a49e8eb9e7a6f67b5ba46d7f
hkwln <p.aul@mail.de>
2025-01-17 23:13:28 +0100

    rename data_visualization2.py to Multiclass_classification.py and enhance classification visualization with accuracy plots and misclassification analysis

--- Multiclass_classification.py
+++ Multiclass_classification.py
@@ -37,11 +37,12 @@ for epoch in tqdm(range(50)):
         # reset gradients to 0
         optim.zero_grad()
         # access data and labels from batch
-        data, gold_labels = next(iter(dataloader))
+        data, gold_labels = batch
         # forward pass
         predictions = classy(data)
         # loss and backward pass
         loss = loss_func(predictions, gold_labels)
+        loss.backward()
         # update network weights
         optim.step()
         # check accuracy (get predicted class for each sample, compare to gold label)
@@ -56,5 +57,45 @@ for epoch in tqdm(range(50)):
     epoch_accs.append(sum(epoch_acc)/len(epoch_acc))
 
 plt.plot(epoch_accs)
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Training Accuracy')
+plt.legend()
+plt.show()
 plt.plot(epoch_losses)
+plt.show()
 
+test_data = TwoClassDataset()
+
+predicted_class_a = []
+predicted_class_b = []
+missclassified_class_a = []
+missclassified_class_b = []
+for i in range(len(test_data)):
+    x, label = test_data[i]
+    pred = classy(x)
+    probs = torch.softmax(pred, dim=0) # note that the softmax is mathematically not needed since it is a monotonic function
+    class_pred = torch.argmax(probs, dim=0)
+  
+    if label != class_pred:
+        if label == 0:
+            missclassified_class_a.append(x.unsqueeze(dim=0))
+        else:
+            missclassified_class_b.append(x.unsqueeze(dim=0))
+    else:
+        if class_pred == 0:
+            predicted_class_a.append(x.unsqueeze(dim=0))
+        else:
+            predicted_class_b.append(x.unsqueeze(dim=0))
+predicted_class_a = torch.cat(predicted_class_a, dim=0) # concatenates tensor along specified, existing dimension, does not create new dimension
+predicted_class_b = torch.cat(predicted_class_b, dim=0) # `stack` is an alternative which creates a new dimension (i.e. we wouldn't have `unsqueeze`d the vectors)
+missclassified_class_a = torch.cat(missclassified_class_a, dim=0)
+missclassified_class_b = torch.cat(missclassified_class_b, dim=0)
+
+fig, ax = plt.subplots()
+ax.scatter(predicted_class_a[:,0], predicted_class_a[:,1], label="class 1", c="darkorange")
+ax.scatter(predicted_class_b[:,0], predicted_class_b[:,1], label="class 2", c="dodgerblue")
+ax.scatter(missclassified_class_a[:,0], missclassified_class_a[:,1], c="darkorange", edgecolors="r")
+ax.scatter(missclassified_class_b[:,0], missclassified_class_b[:,1], c="dodgerblue", edgecolors="r")
+ax.legend()
+plt.show()
