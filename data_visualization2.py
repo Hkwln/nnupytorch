e91cc070213d8fea959f65d35d2c18a800402c1d
hkwln <p.aul@mail.de>
2025-01-17 22:35:09 +0100

    changed name from tqdm to data_visualization2

--- data_visualization2.py
+++ data_visualization2.py
@@ -1,17 +1,21 @@
-from tqdm import tqdm
+from data_visualization2 import tqdm
 import torch
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, Dataset, Subset
+from data_visualization import train_subset
 
 class Classifier(torch.nn.Module):
     def __init__(self):
         super().__init__()
         # define the layers
-        return 0
+        self.layer1 = torch.nn.Linear(2,5)
+        self.layer2 = torch.nn.Linear(5,2)
             
     def forward(self, x):
         # define the forward pass
-        return x
+        y1 = torch.relu(self.layer1(x))
+        y2 = self.layer2(y1)
+        return y2
 # instantiate model, loss criterion and optimizer
 classy = Classifier()
 loss_func = torch.nn.CrossEntropyLoss()
@@ -20,7 +24,7 @@ optim = torch.optim.SGD(params=classy.parameters(), lr=0.01)
 # store epoch metrics
 epoch_accs = []
 epoch_losses = []
-dataloader = DataLoader(dataset=train_data, batch_size=25, shuffle=True)
+dataloader = DataLoader(dataset=train_subset, batch_size=25, shuffle=True)
 
 # epoch loop
 for epoch in tqdm(range(50)):
@@ -30,15 +34,15 @@ for epoch in tqdm(range(50)):
     # mini-batch loop for one epoch
     for batch in dataloader:
         # reset gradients to 0
-        torch.optim.zero_grad()
+        optim.zero_grad()
         # access data and labels from batch
-        
+        data, gold_labels = next(iter(dataloader))
         # forward pass
-       
+        predictions = classy(data)
         # loss and backward pass
-        
+        loss = loss_func(predictions, gold_labels)
         # update network weights
-        
+        optim.step()
         # check accuracy (get predicted class for each sample, compare to gold label)
         category_probs = torch.softmax(predictions, dim=1) # sums up to 1 for each sample
         category_labels = torch.argmax(category_probs, dim=1) # extract most likely label
