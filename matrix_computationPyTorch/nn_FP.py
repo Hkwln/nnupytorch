6f690c49194b9f8cab7aa006a844704fdb28d290
hkwln <p.aul@mail.de>
2025-01-20 21:02:13 +0100

    fixed a typo

--- matrix_computationPyTorch/nn_FP.py
+++ matrix_computationPyTorch/nn_FP.py
@@ -1,6 +1,6 @@
 import torch
 
-class SimpleForwardPass(torch.nn.Module)
+class SimpleForwardPass(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.layer1 = torch.nn.Linear(4, 3)
@@ -10,10 +10,12 @@ class SimpleForwardPass(torch.nn.Module)
         
         #defining custom parameters for the model
         self.param_w_1 = torch.nn.Parameter([0.5, -0.2, 0.5, 0.4 ],[-0.3, 0.8,-0.5,0.2],[0.2,-0.1,0.3,-0.4])
-        self.param_b_1 = torch.nn.Parameter([],[],[])
-        self.param_w_2 = torch.nn.Parameter( )
-        self.param_b_2 = torch.nn.Parameter()
+        self.param_b_1 = torch.nn.Parameter([0.1],[0.0],[-0.2])
+        self.param_w_2 = torch.nn.Parameter([0.3,-0.6,0.7],[0.5,-0.8,0.1] )
+        self.param_b_2 = torch.nn.Parameter([0.2],[-0.3])
 
     def forward(self, x):
-
+        x = torch.relu(self.layer1(x))
+        x = self.layer2(x)
+        x = torch.relu(self.layer3(x))
         return x
