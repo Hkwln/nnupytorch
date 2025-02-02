3f5a7bd0a34fa40344e110be9763219b6a700c9b
hkwln <p.aul@mail.de>
2025-02-02 13:33:25 +0100

    implement sentiment prediction and validation for user input in interactive_predictions.py

--- sentiment_analysis(SA)/interactive_predictions.py
+++ sentiment_analysis(SA)/interactive_predictions.py
@@ -3,26 +3,48 @@ from IPython.display import display
 from SA import model2, map_text_to_indices
 import torch
 
-#todo a predict for the provided sentence using our trained model
-#hints:
-# - you need to convert the input sentence to token ids, using the same mapping as for the training.
-#   Can you re-use something to accomplish this?
-# - since we only have a single input, we don't need batching nor a dataloader
-# . We don't need the gradients from the model
-sentence_widget = widgets.Text(
-    value="This movie is terrible",
-    placeholder="Type something",
-    description="Sentence:",
-    disabled=False,
-)
-display(sentence_widget)
+
+#here we change the char input sentence into tokens and finaly into a torch tensor and feed into the model
+#input: sentence in char
+#output: tensor (for example: tensor([[ 0.5330, -0.3757]])) 
+#        which represents the sentiment of the input sentence
 def predict_sentence(sentence:str):
     token_ids = map_text_to_indices(sentence)
     token_ids = torch.tensor([token_ids],dtype=torch.long)
     with torch.no_grad():
-        predictions = model2(sentence)
+        predictions = model2(token_ids)
     return predictions
 
-sentence = sentence_widget.value
-predictions = predict_sentence(sentence)
-display(predictions)
+#here we define a function where we have as a input a tensor like tensor([[ 0.5330, -0.3757]]), which represents the sentiment of the input sentence; 
+#input: tensor (for example: tensor([[ 0.5330, -0.3757]]))
+#output:translate the tensor into positive, negative, neutral / probability
+def get_sentiment(tensor):
+    #softmax function:
+    #compute exponentials of the logits
+    exp = torch.exp(tensor)
+    #compute the sum of the logits to normalize
+    total = exp.sum()
+    #normalize the exponentials to get the probabilities
+    probs = exp / total
+    #define if the text is positiv, negativ or neutral
+    #positiv: [[under 50%][over 50%]]
+    #negative: [[over 50%][under 50%]]
+    #neutral:  both under or both over 50%
+    for bad,good in probs:
+        if bad <0.5 and good >0.5:
+            print("positive")
+        elif bad >0.5 and good <0.5:
+            print("negative")
+        else: print("neutral")
+
+    return probs
+
+ipsentence = input("give me an input sentence:")
+#validate the input sentence
+#Explanation: strip() function removes all white spaces
+if not ipsentence.strip():
+    raise ValueError("Inputsentence cannot be empty.")
+
+predictions = predict_sentence(ipsentence)
+display(get_sentiment(predictions))
+
