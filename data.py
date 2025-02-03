593ba8975a05da7427c8b3053b07691600ba8079
hkwln <p.aul@mail.de>
2025-02-03 12:17:06 +0100

    added batching for the data

--- data.py
+++ data.py
@@ -24,16 +24,52 @@ chars = sorted(list(set(all_text)))
 vocab_size = len(chars)
 print(vocab_size)
 print(chars)
+
 # Create a mapping from characters to integers
-char_to_int ={char: i for i, char in enumerate(chars)}
+char_to_int = {char: i for i, char in enumerate(chars)}
 int_to_char = {i: char for i, char in enumerate(chars)}
 encode = lambda s: [char_to_int[c] for c in s]
 decode = lambda a: ''.join([int_to_char[i] for i in a])
 
 #preparing the data for the model
 data = torch.tensor(encode(all_text), dtype=torch.int64)
-n = 0,85*len(data) # 85% of the data is used for training, rest for testing
-train_data = data[n:]
-test_data = data[:n]
 
-#now I need to do the nnupytorch repository first, to get into using torch more
+# 85% of the data is used for training, rest for testing
+n = int(0.85 * len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+block_size = 8
+train_data[:block_size+1]
+
+x = train_data[:block_size]
+y = train_data[1:block_size+1]
+for t in range(block_size):
+    context = x[:t+1]
+    target = y[t]
+    print(f"when input is{context} the target:{target}")
+
+
+batch_size = 4
+block_size = 5
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+1+block_size] for i in ix])
+    return x, y
+
+xb, yb = get_batch('train')
+print('inputs:')
+print(xb.shape)
+print(xb)
+print('targets')
+print(yb.shape)
+print(yb)
+
+print('----')
+
+for b in range(batch_size):
+    for t in range(block_size):
+        print(f"Batch {b}, Token {t}: input={xb[b, t]} target={yb[b, t]}")
