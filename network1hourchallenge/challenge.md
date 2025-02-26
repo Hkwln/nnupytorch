ff2cc3a4466a6449ee9e5f5db0fec22dc7ab1d35
Paulxocmd <sibila.paul2@gmail.com>
2025-02-26 20:59:42 +0100

    created challenge

---
+++ network1hourchallenge/challenge.md
@@ -0,0 +1,4 @@
+<h3>Here is the challenge: 
+
+use the Math_symbols library to recognize different symbols like circle, triangle ...
+
---
+++ network1hourchallenge/main.py
@@ -0,0 +1,17 @@
+import torch
+from datasets import load_dataset
+
+ds = load_dataset("prithivMLmods/Math-symbols")
+
+print(ds)
+#shape of the dataset:
+
+#changing the pixels into a tensor
+
+
+#model definition (maybe CNN, research)
+
+
+#training the model
+
+#testing the model with the validation set
