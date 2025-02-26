f1859a9fa5a2138e6ba11f12b3cfe1042272dfd8
Paulxocmd <sibila.paul2@gmail.com>
2025-02-26 21:37:39 +0100

    accdc model

--- network1hourchallenge/main.py
+++ network1hourchallenge/main.py
@@ -6,12 +6,18 @@ ds = load_dataset("prithivMLmods/Math-symbols")
 print(ds)
 #shape of the dataset:
 
-#changing the pixels into a tensor
+#changing the pixels into a tensor and refactoring the dataset
 
 
 #model definition (maybe CNN, research)
-
-
+class simplecnn(torch.nn.Module):
+    def __init__(self):
+        super(simplecnn, self).__init__
+        self.cnn = torch.nn.Conv2d()
+        self.pooling = torch.nn.MaxPool2d()
+        self.fcl = torch.nn.Linear()
+    def forward(self, x):#
+        return x
 #training the model
 
 #testing the model with the validation set
