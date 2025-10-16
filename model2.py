4c3a7789d649cb354acf2d15223d4594c5695f4f
Paulxocmd <p.aul@mail.de>
2025-10-16 13:03:52 +0200

    added another model, where i have the inspiration from a book

---
+++ model2.py
@@ -0,0 +1,10 @@
+import torch
+
+# this is yet to be built, i want to take another approach on the model and want to compare the results
+class PhraseModel(torch.nn.Module):
+    def __init__(self, embed_size, dict_size, hid_size)
+                 super(PhraseModel, self).__init__()
+
+
+
+
