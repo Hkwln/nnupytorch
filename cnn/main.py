db16ace58b84da96781bb0e6830e8fbf2836010a
Paulxocmd <p.aul@mail.de>
2025-10-20 20:59:04 +0200

    added plotting thru tensorboard

--- cnn/main.py
+++ cnn/main.py
@@ -5,6 +5,7 @@ from model import simplecnn
 from datasethandler import MathSymbolsDataset
 from torch.utils.data import DataLoader
 import os
+from tensorboard import SummaryWriter
 current_dire = os.path.dirname(os.path.abspath(__file__))
 path = os.path.join(current_dire,"model.pt")
 #               data preperation
@@ -29,7 +30,7 @@ validation_dataset = MathSymbolsDataset(x_val, y_val, transform)
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 val_loader= DataLoader(validation_dataset, batch_size=32, shuffle=True)
 
-
+writer =SummaryWriter('runs/cnn')
 model = simplecnn()
 
 loss_fn = torch.nn.CrossEntropyLoss()
@@ -37,14 +38,17 @@ optimizer = torch.optim.Adam(params = model.parameters(),lr = 1e-4)
 losses_train, losses_val = [],[]
 accuracy_train, accuracy_val = [],[]
 num_epochs = 2
+counter: int = 0
 for epoch in range(num_epochs):
     print(f"Epoch [{epoch + 1}/{num_epochs}]")
     for image, labels in train_loader:
+        counter +=1
         outputs = model(image)
         loss = loss_fn(outputs, labels)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        writer.add_scalar('loss', loss, counter)
     #after a epoch, evaluate my model on the validation_dataset
     #use torch.eval() and torch.no_grad()
     # Calculate the validatioin loss and accuracy
@@ -52,11 +56,14 @@ for epoch in range(num_epochs):
     val_loss = 0
     correct = 0
     total = 0
+    counter = 0
     with torch.no_grad():
         for image, labels in val_loader:
             out = model(image)
+            counter +=1
             
-            
+            writer.add_scalar('validation', val_accuracy, counter)
+            writer.add_scalar('validation loss', val_loss, counter)
             loss = loss_fn(out, labels)
             val_loss = loss.item() *labels.size(0)
             _, predictions = torch.max(out, 1)
@@ -72,8 +79,12 @@ for epoch in range(num_epochs):
     #total correct/total samples = accuracy
     val_accuracy = correct/total
     print(f"validation lost: {val_loss}, validation accuracy={val_accuracy}")
+    #logging validation metrics, val accuracy, val loss
     model.train()# go back into training mode
-
+    # every few epochs store params in histogram:
+    for name, param in model.parameters():
+        writer.add_histogram('model_state', param.data().cpu().numpy(), epoch)
+writer.close()
     
 #safe the the trained model inside a .pt format
 current_state = {
